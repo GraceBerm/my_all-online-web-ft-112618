76382c11a784256dae3743d507945a47b32ea008
Grace Berman <grace.g.berman@gmail.com>
2020-04-11 15:04:00 +0000

    Automatically backed up by Learn

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -1,9 +1,16 @@
 require 'pry'
 
 def my_all?(collection)
+block_return_values = []
  i = 0
  while i < collection.length
-   yield(collection[1])
+  block_return_values << yield(collection[1])
    i += 1
  end
+
+  if block_return_values.include?(false)
+    false
+  else
+    true
+  end
 end
